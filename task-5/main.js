3ee255462fd05f31eb6260196326d25bdd6360bd
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-01 20:29:45 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,10 +1,21 @@
-// Use this matrix for solution
-
-let matrix = [
-    [134, 475, 30, 424],
-    [303, 151, 419, 235],
-    [248, 166, 90, 42],
-    [318, 237, 184, 36]
+let mtr = [
+  [134, 475, 30, 424],
+  [303, 151, 419, 235],
+  [248, 166, 90, 42],
+  [318, 237, 184, 36],
 ];
+let sum1 = 0;
+let sum2 = 0;
+for (i = 0; i < mtr.length; i++) {
+  for (j = 0; j < mtr[i].length; j++)
+    if (i == j) {
+      sum1 = sum1 + mtr[i][j];
+    }
+    else if (j == mtr[i].length - (1+i)) {
+      sum2 = sum2 + mtr[i][j];
+      
+    }
+}
 
-// Write code here. 
+console.log(sum1);
+console.log(sum2);
