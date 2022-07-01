d5b02c75e3d712ea3545cb589b1280e6a475d86d
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-01 20:39:46 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,15 +1,21 @@
-// Use this matrices for solution
+let mtr1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-let matrix_1 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]; 
+let mtr2 = [
+  [1, 0, 1],
+  [0, 1, 0],
+  [1, 0, 1],
+];
+let sum = 0;
 
-let matrix_2 = [
-    [1, 0, 1],
-    [0, 1, 0],
-    [1, 0, 1]
-]; 
-
-// Write code here. 
+for (i = 0; i < mtr2.length; i++) {
+  for (j = 0; j < mtr2[i].length; j++) {
+    if (mtr2[i][j] == 1) {
+      sum = sum+mtr1[i][j];
+    }
+  }
+}
+console.log(sum)
