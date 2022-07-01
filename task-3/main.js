beb5eb031cac59a267071a6cba9eb65ae2cd537d
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-01 19:51:29 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,4 +1,15 @@
-// Use this array for solution
-let array = [3, 5, -7, 7, 5, -9, -4]
-
-// Write code here. 
+let array = [3, 5, -7, 7, 5, -9, -4];
+let arr = [];
+let count;
+for (i = 0; i < array.length; i++) {
+  count = 0;
+  for (j = 0; j < array.length; j++) {
+    if (array[i] === array[j]) {
+      count++;
+    }
+  }
+  if (!(count > 1)) {
+    arr.push(array[i]);
+  }
+}
+console.log(arr);
