f7a2311a5776f40d12e27edce85a70959f62c7bd
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-01 19:59:00 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,13 @@
-// Use this matrix for solution
-
-let matrix = [
-    [4, -2, 5],
-    [1, -4, -12],
-    [0, 1, -3]
+let mtr = [
+  [4, -2, 5],
+  [1, -4, -12],
+  [0, 1, -3]
 ];
-
-// Write code here. 
+let sum = 0;
+for (i = 0; i<mtr.length; i++) {
+  for( j=0; j<mtr[i].length; j++)
+    if(mtr[i][j]>0){
+      sum= sum+mtr[i][j];
+    }
+  }
+  console.log(sum)
